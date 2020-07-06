65a6a2c7e71e317de50e9594dde2f9bbfb66e888
jktulord <olden112002@mail.ru>
2020-07-06 17:03:53 +0300

    0.3.2

--- cogs/help.py
+++ cogs/help.py
@@ -18,19 +18,19 @@ class help(commands.Cog):
 
         embed.set_author(name=functs.NAME)
 
-        embed.add_field(name='!help', value="Показывает это сообщение", inline=False)
+        embed.add_field(name='---!help', value="Показывает это сообщение", inline=False)
 
-        embed.add_field(name='!getHots', value="Получение Роли Хотсеры", inline=False)
-        embed.add_field(name='!getNews', value="Новостная Подписка", inline=False)
-        embed.add_field(name='!pokonyam', value="Зовет всех Хотсеров в Хотс", inline=False)
-        embed.add_field(name='!randomHero',
+        embed.add_field(name='---!getHots---', value="Получение Роли Хотсеры", inline=False)
+        embed.add_field(name='---!getNews---', value="Новостная Подписка", inline=False)
+        embed.add_field(name='---!pokonyam---', value="Зовет всех Хотсеров в Хотс", inline=False)
+        embed.add_field(name='---!randomHero---',
                         value="Выдает случайного героя. Для работы требуются Теги. (список всех тегов: Все, ААтакеры, "
                               "Маги, Жир, Танки, Брузяхи, Лекари, Поддержка, Ебоклаки)",
                         inline=False)
 
-        embed.add_field(name='!infinity', value="Показывает Бесконечность", inline=False)
-        embed.add_field(name='!aleksey', value="Отправляет сообщение СТУКОВ", inline=False)
-        embed.add_field(name='!stukhov', value="Отправляет сообщение АЛЕКСЕЙ", inline=False)
+        embed.add_field(name='---!infinity---', value="Показывает Бесконечность", inline=False)
+        embed.add_field(name='---!aleksey---', value="Отправляет сообщение СТУКОВ", inline=False)
+        embed.add_field(name='---!stukhov---', value="Отправляет сообщение АЛЕКСЕЙ", inline=False)
 
         await ctx.send(embed = embed)
 
